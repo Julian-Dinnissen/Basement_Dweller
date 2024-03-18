eeb32579350f3d341ea0f0ba010cd8af5718065e
Julian <113358009+Julian-Dinnissen@users.noreply.github.com>
2024-03-18 15:15:17 +0100

    Fix role permissions and add audit log

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.guild != client.get_guild(1065705816105160704):
+        return
+
+    await check_role(message)
+
+    await print_audit_log(message)
+    entry = await client.wait_for('audit_log_entry', check=lambda entry: entry.guild == message.guild)
+    print(entry)
+    
 
 # a fucntion that prints the permissions of a user: 398769543482179585 in guild: 1073971508654899280
 @client.event
@@ -32,27 +42,39 @@ async def on_ready():
     role = discord.utils.get(guild.roles, name="discord mod (fat mofo)")
     gestapo = discord.utils.get(guild.roles, name="Gestapo")
 
-    perms_dict = {}
+    # permissions = discord.Permissions(
+    #     manage_channels=True,
+    #     manage_guild=True,  # This is the "manage server" permission
+    #     manage_roles=True,
+    #     manage_webhooks=True,
+    #     read_message_history=True,
+    #     kick_members = True,
+    #     mute_members = True,
+    #     deafen_members = True
+    # )
+    # permissions.update(manage_messages = True)
+    # await role.edit(reason = None, permissions=permissions)
+
+
+async def check_role(message):
+    role = discord.utils.get(message.guild.roles, name="Gestapo")
+    if role in message.author.roles:
+        print(f"{message.author} has the role {role.name}")
+    else:
+        print(f"{message.author} does not have the role {role.name}")
+
+    roles = [role for role in message.guild.roles if not role.managed]
+    for role in roles:
+        print(role.name)
+
+async def save_perms(role):
+    dict1 = {}
+    for perm in role.permissions:
+        dict1[perm[0]] = perm[1]
     
-    # give the permissions:
-# manage channels, manage server, manages roles, manage webhooks, MANAGE_GUILD_EXPRESSIONS, CREATE_GUILD_EXPRESSIONS, VIEW_AUDIT_LOG, READ_MESSAGE_HISTORY
-# test for the 
-    permissions = discord.Permissions(
-        manage_channels=True,
-        manage_guild=True,  # This is the "manage server" permission
-        manage_roles=True,
-        manage_webhooks=True,
-        read_message_history=True,
-        kick_members = True,
-        mute_members = True,
-        deafen_members = True
-    )
-    permissions.update(manage_messages = True)
-    await role.edit(reason = None, permissions=permissions)
 
-    for perm in gestapo.permissions:
-        perms_dict[perm[0]] = perm[1]
-
-    print(perms_dict["manage_threads"])
+async def print_audit_log(message):
+    async for entry in message.guild.audit_logs(limit=10):
+        print(entry)
 
 client.run(TOKEN)
