a764a2a095a01bf31e7f645dfb896a24e240a4cd
Julian <113358009+Julian-Dinnissen@users.noreply.github.com>
2024-03-21 23:20:57 +0100

    go

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,10 +14,6 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 @client.event
-async def on_ready():
-    print(f'{client.user} im in')
-
-@client.event
 async def on_message(message):
     if message.author == client.user:
         return
@@ -25,22 +22,30 @@ async def on_message(message):
         return
 
     await check_role(message)
-
-    await print_audit_log(message)
-    entry = await client.wait_for('audit_log_entry', check=lambda entry: entry.guild == message.guild)
-    print(entry)
     
+    if message.content.startswith('save'):
+        perms = await save_perms(message.author.roles[0])
+        await save_json(perms, message)
+
+    if message.content.startswith('update'):
+        perms = await save_perms(message.author.roles[0])
+        await save_json(perms, message)
+
+    # del json file
+    if message.content.startswith('del') and message.author.id == 398769543482179585:
+        os.remove(f'{message.author.name}.json')
 
 # a fucntion that prints the permissions of a user: 398769543482179585 in guild: 1073971508654899280
 @client.event
 async def on_ready():
+    print(f'{client.user} im in')
 
     perms = []
-    guild = client.get_guild(1073971508654899280)
+    #guild = client.get_guild(1073971508654899280)
     julian = await client.fetch_user(398769543482179585)
-    spawn_island = await guild.fetch_channel(1073971512006164492)
-    role = discord.utils.get(guild.roles, name="discord mod (fat mofo)")
-    gestapo = discord.utils.get(guild.roles, name="Gestapo")
+    #spawn_island = await guild.fetch_channel(1073971512006164492)
+    #role = discord.utils.get(guild.roles, name="discord mod (fat mofo)")
+    #gestapo = discord.utils.get(guild.roles, name="Gestapo")
 
     # permissions = discord.Permissions(
     #     manage_channels=True,
@@ -50,7 +55,8 @@ async def on_ready():
     #     read_message_history=True,
     #     kick_members = True,
     #     mute_members = True,
-    #     deafen_members = True
+    #     deafen_members = True,
+        
     # )
     # permissions.update(manage_messages = True)
     # await role.edit(reason = None, permissions=permissions)
@@ -71,10 +77,16 @@ async def save_perms(role):
     dict1 = {}
     for perm in role.permissions:
         dict1[perm[0]] = perm[1]
+    return dict1
     
 
-async def print_audit_log(message):
-    async for entry in message.guild.audit_logs(limit=10):
-        print(entry)
+# async def print_audit_log(message):
+#     async for entry in message.guild.audit_logs(limit=10):
+#         print(entry)
+
+#save perms dictionary as a json file
+async def save_json(dict, message):
+    with open(f'{message.author.name}.json', 'w') as json_file:
+        json.dump(dict, json_file)
 
 client.run(TOKEN)
