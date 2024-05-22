3fa25eb736e1d2c354faccf3e5bffc9779580c18
Julian <113358009+Julian-Dinnissen@users.noreply.github.com>
2024-05-22 15:30:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,47 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.guild != client.get_guild(1065705816105160704):
-        return
 
-    await check_role(message)
+
+
+
+
+async def check_role(message) -> None:
+    role = discord.utils.get(message.guild.roles, name="Gestapo")
+    if role in message.author.roles:
+        print(f"{message.author} has the role {role.name}")
+    else:
+        print(f"{message.author} does not have the role {role.name}")
+
+    roles = [role for role in message.guild.roles if not role.managed]
+    for role in roles:
+        print(role.name)
+
+async def save_perms(role) -> dict:
+    dict1 = {}
+    for perm in role.permissions:
+        dict1[perm[0]] = perm[1]
+    return dict1
     
+
+async def print_audit_log(message) -> None:
+    async for entry in message.guild.audit_logs(limit=10):
+        print(entry)
+
+#save perms dictionary as a json file
+async def save_json(dict: dict, message) -> None:
+    with open(f'{message.author.name}.json', 'w') as json_file:
+        json.dump(dict, json_file)
+
+
+async def check_env(message) -> bool:
+    if message.author == client.user:
+        return False
+    
+    if message.guild == client.get_guild(1041110850666246185):
+        return True
+
+async def json_creation(message) -> None:
     if message.content.startswith('save'):
         perms = await save_perms(message.author.roles[0])
         await save_json(perms, message)
@@ -35,17 +66,13 @@ async def on_message(message):
     if message.content.startswith('del') and message.author.id == 398769543482179585:
         os.remove(f'{message.author.name}.json')
 
-# a fucntion that prints the permissions of a user: 398769543482179585 in guild: 1073971508654899280
-@client.event
-async def on_ready():
-    print(f'{client.user} im in')
-
+async def set_perms() -> None:
     perms = []
-    #guild = client.get_guild(1073971508654899280)
+    guild = client.get_guild(1073971508654899280)
     julian = await client.fetch_user(398769543482179585)
-    #spawn_island = await guild.fetch_channel(1073971512006164492)
-    #role = discord.utils.get(guild.roles, name="discord mod (fat mofo)")
-    #gestapo = discord.utils.get(guild.roles, name="Gestapo")
+    spawn_island = await guild.fetch_channel(1073971512006164492)
+    role = discord.utils.get(guild.roles, name="discord mod (fat mofo)")
+    gestapo = discord.utils.get(guild.roles, name="Gestapo")
 
     # permissions = discord.Permissions(
     #     manage_channels=True,
@@ -56,37 +83,21 @@ async def on_ready():
     #     kick_members = True,
     #     mute_members = True,
     #     deafen_members = True,
+    #     move_members = True
         
     # )
     # permissions.update(manage_messages = True)
     # await role.edit(reason = None, permissions=permissions)
 
+@client.event
+async def on_message(message):
+    if not await check_env(message): # checks if the message is send in the correct guild and not by the bot
+        return
 
-async def check_role(message):
-    role = discord.utils.get(message.guild.roles, name="Gestapo")
-    if role in message.author.roles:
-        print(f"{message.author} has the role {role.name}")
-    else:
-        print(f"{message.author} does not have the role {role.name}")
+# a fucntion that prints the permissions of a user: 398769543482179585 in guild: 1073971508654899280
+@client.event
+async def on_ready():
+    print(f'{client.user} im in')
 
-    roles = [role for role in message.guild.roles if not role.managed]
-    for role in roles:
-        print(role.name)
-
-async def save_perms(role):
-    dict1 = {}
-    for perm in role.permissions:
-        dict1[perm[0]] = perm[1]
-    return dict1
-    
-
-# async def print_audit_log(message):
-#     async for entry in message.guild.audit_logs(limit=10):
-#         print(entry)
-
-#save perms dictionary as a json file
-async def save_json(dict, message):
-    with open(f'{message.author.name}.json', 'w') as json_file:
-        json.dump(dict, json_file)
 
 client.run(TOKEN)
